be7bedabec1e56bbb19be2b845604bad3067e4b3
nateg <n8gallagher@gmail.com>
2020-08-18 16:16:39 -0700

    prepare for heroku push

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,16 +2,22 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        
+        if @user.save
+            login!(@user)
+            redirect_to api_user_url
+        else
+            render[:errors] = ['something went wrong']
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render :show
     end
 
     def new
         @user = User.new
         render :new
-    end
-
-    def update
-        @user = User.find(params[:id])
     end
 
     def edit
