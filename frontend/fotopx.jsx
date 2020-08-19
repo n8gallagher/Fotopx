7bc75c47ede0c90c753a24a76ea391a7d61079b7
nateg <n8gallagher@gmail.com>
2020-08-18 17:35:07 -0700

    starting front end for auth

---
+++ frontend/fotopx.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {HashRouter, Route } from 'react-router-dom'
+
+import configureStore from './store'
