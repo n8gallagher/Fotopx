0ffd98b8729c770555a0965abdd4a2eb2520074e
nateg <n8gallagher@gmail.com>
2020-10-23 15:37:50 -0700

    Added photo_show

---
+++ frontend/components/photo/photo_show.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class PhotoShow extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+}
+
+
+export default PhotoShow;
---
+++ frontend/components/photo/photo_show_container.js
@@ -0,0 +1,13 @@
+import {connect} from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import PhotoShow from './photo_show'
+
+const mapStateToProps = (state) => ({
+  photo: 
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return null;
+}
+
+export default connect(mapDispatchToProps, mapStateToProps)(PhotoShow);
